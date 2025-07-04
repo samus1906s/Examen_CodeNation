419ee5c076dc9f4f645f986810996478739e1ee1
samus1906s <sacerdasce@est.utn.ac.cr>
2025-07-04 15:55:23 -0600

    Lista Ciudadanos
    Se creo la lista de ciudadano

---
+++ src/Ciudadanos/ListaCiudadano.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ciudadanos;
+
+/**
+ *
+ * @author Student
+ */
+public class ListaCiudadano implements MetodosComportamientoCiudadano {
+
+        private Cuidadano ciudadanos[];
+    
+    
+    public boolean add(Object t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
+    public boolean remove(Object t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
+    public Object find(Object id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+    public int ListaFuncionarios(String IngresadoCiudadano){
+        int contador = 0;
+        for(int i = 0; i<ciudadanos.length; i++){
+            if(ciudadanos[i] != null && ciudadanos[i].getId().(id){
+                contador = contador + 1;
+                return contador;
+            }
+        }
+            return 0;
+    }
+        
+ 
+}    
+
+
